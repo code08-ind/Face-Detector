65a5802cc39006a350bfd4114736bbb2eddbc3dc
Aryan <gargaryan82000@gmail.com>
2021-06-16 20:03:55 +0530

    Added All The Files

---
+++ Face_Detector.cpp
@@ -0,0 +1,32 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+///////////////////// Face Detection ///////////////////////
+
+void main() {
+	string path = "Resources/test.png";
+	Mat img = imread(path);//mat is a matrix data type which is used with images
+
+	CascadeClassifier faceCascade; 
+	faceCascade.load("Resources/haarcascade_frontalface_default.xml");
+
+	if (faceCascade.empty()) {
+		cout << "XML File Not Loaded" << endl;
+	}
+
+	vector<Rect> faces;
+	faceCascade.detectMultiScale(img, faces, 1.1, 10);
+
+	for (int i = 0; i < faces.size(); i++) {
+		rectangle(img, faces[i].tl(), faces[i].br(), Scalar(255, 0, 255), 3);
+	}
+
+	imshow("image", img);
+	waitKey(0);//it will only close when we press the close button
+}
